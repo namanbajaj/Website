3fab63962d91fc85727338a8c2ce9affdd2a29c9
namanbajaj <namanbajaj05@gmail.com>
2022-06-01 12:33:17 -0400

    styling

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Navbar from './components/Navbar/Navbar'
+import Portfolio from './components/Portfolio/Portfolio'
+import Welcome from './components/Welcome/Welcome'
+
+
+const App = () => {
+  return (
+    <>
+        <Navbar />
+        <Welcome />
+        <About />
+        <Portfolio />
+        <Contact />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './welcome.css'
+
+const Welcome = () => {
+  return (
+    <div>Welcome</div>
+  )
+}
+
+export default Welcome
