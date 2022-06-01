7cacde238a5b2c15dec96acc79ee22e1f22a48fa
namanbajaj <namanbajaj05@gmail.com>
2022-06-01 16:42:28 -0400

    picture

--- src/components/Welcome/Socials.jsx
+++ src/components/Welcome/Socials.jsx
@@ -3,9 +3,9 @@ import { BsLinkedin, BsGithub } from 'react-icons/bs'
 
 const Socials = () => {
     return (
-        <div className='header__socials'>
-            <a href="https://www.linkedin.com/in/namanbajaj" target="_blank"><BsLinkedin></BsLinkedin></a>
-            <a href="https://github.com/namanbajaj" target="_blank"><BsGithub></BsGithub></a>
+        <div className='socials'>
+            <a href="https://www.linkedin.com/in/namanbajaj" target="_blank" rel="noopener noreferrer"><BsLinkedin></BsLinkedin></a>
+            <a href="https://github.com/namanbajaj" target="_blank" rel="noopener noreferrer"><BsGithub></BsGithub></a>
         </div>
     )
 }
