9e1560d08d0af5bf1d339dc42119730b498b570a
namanbajaj <namanbajaj05@gmail.com>
2022-06-07 17:43:04 -0400

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 import Portfolio from './components/Portfolio/Portfolio'
 import Welcome from './components/Welcome/Welcome'
@@ -14,6 +15,7 @@ const App = () => {
         <About />
         <Portfolio />
         <Contact />
+        <Footer/>
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './footer.css'
+
+import {FaFacebookF, FaInstagram } from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+      <footer>
+          <a href="#" className='footer__logo'>Designed by Naman Bajaj</a>
+          <ul className='permalinks'>
+              <li><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#portfolio">Portfolio</a></li>
+              <li><a href="#contact">Contact</a></li>
+          </ul>
+
+          <div className='footer__socials'>
+              <a href="https://www.facebook.com/profile.php?id=100078176513403" target='_blank'><FaFacebookF/></a>
+              <a href="https://www.instagram.com/naman.bajaj_/" target='_blank'><FaInstagram/></a>
+          </div>
+
+          <div className='footer__copyright'>
+              <small>&copy; Naman Bajaj. All rights reserved.</small>
+          </div>
+      </footer>
+  )
+}
+
+export default Footer
