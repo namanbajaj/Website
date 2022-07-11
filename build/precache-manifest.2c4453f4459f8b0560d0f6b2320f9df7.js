cc892ae8298289a48a72ce318f0bfa524fb24843
namanbajaj <namanbajaj05@gmail.com>
2022-07-10 21:15:44 -0400

    added new resume

---
+++ build/precache-manifest.2c4453f4459f8b0560d0f6b2320f9df7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "d54bda0c9038c9cbcff7",
+    "url": "/Website/static/css/main.220a7649.chunk.css"
+  },
+  {
+    "revision": "d54bda0c9038c9cbcff7",
+    "url": "/Website/static/js/main.d54bda0c.chunk.js"
+  },
+  {
+    "revision": "82d1d8c630b188d028f3",
+    "url": "/Website/static/css/1.db7b55d0.chunk.css"
+  },
+  {
+    "revision": "82d1d8c630b188d028f3",
+    "url": "/Website/static/js/1.82d1d8c6.chunk.js"
+  },
+  {
+    "revision": "9ba5c3127d442ea08398",
+    "url": "/Website/static/js/2.9ba5c312.chunk.js"
+  },
+  {
+    "revision": "dd897416a3ff4a09945c",
+    "url": "/Website/static/js/runtime~main.dd897416.js"
+  },
+  {
+    "revision": "070ebcf8d01f3b1f6222b49ca3a1ba09",
+    "url": "/Website/static/media/websiteportfolio.070ebcf8.png"
+  },
+  {
+    "revision": "81c1f1051424fb719cdbd995ed47bbf3",
+    "url": "/Website/static/media/PAPhoto.81c1f105.png"
+  },
+  {
+    "revision": "b5a7fa3b3735082fc648d680b64d70f1",
+    "url": "/Website/static/media/PADemo.b5a7fa3b.mp4"
+  },
+  {
+    "revision": "6c84e5ce193f437df26cf915941dcb7c",
+    "url": "/Website/static/media/PDPhoto.6c84e5ce.png"
+  },
+  {
+    "revision": "a1f670a95e3e62f13e0f851be055f0fa",
+    "url": "/Website/static/media/PDDEMO.a1f670a9.mp4"
+  },
+  {
+    "revision": "0a56bb101383a0c1ac16ab6a8384e5a5",
+    "url": "/Website/static/media/CPhoto.0a56bb10.png"
+  },
+  {
+    "revision": "47c4c8579842d9b929fad3df9d53fdeb",
+    "url": "/Website/static/media/CDemo.47c4c857.mp4"
+  },
+  {
+    "revision": "dff7cc5e515c5e16786a6f6586b288d1",
+    "url": "/Website/static/media/SLPhoto.dff7cc5e.png"
+  },
+  {
+    "revision": "c62cce648bfaeaf160e7dfc704ee3ece",
+    "url": "/Website/static/media/SLDemo.c62cce64.mp4"
+  },
+  {
+    "revision": "66c6a0adbe54ebd4819c5c0e1179a934",
+    "url": "/Website/static/media/CGPhoto.66c6a0ad.png"
+  },
+  {
+    "revision": "ac7429b4b49aa03aab5dccf61d086998",
+    "url": "/Website/static/media/CGDemo.ac7429b4.mp4"
+  },
+  {
+    "revision": "af5a5c07c2e9a12a71ead1a4cca7eff5",
+    "url": "/Website/static/media/Resume.af5a5c07.pdf"
+  },
+  {
+    "revision": "4e2fde2e8e97859756a3e018bc579795",
+    "url": "/Website/static/media/me.4e2fde2e.jpeg"
+  },
+  {
+    "revision": "b9688f708d399600306f4a179e50cdb2",
+    "url": "/Website/index.html"
+  }
+];
