29820fe9af0d4d23dc2b571bd2003edea6ffc092
Naman Bajaj <namanbajaj05@gmail.com>
2023-02-02 01:57:18 -0500

    small fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
         <footer>
             <a href="#" className='footer__logo'>Designed by Naman Bajaj</a>
             <ul className='permalinks'>
-                <li><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#experience">Experience</a></li>
-                <li><a href="#languages">Languages</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#certifications">Certifications</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li className='link'><a href="#">Home</a></li>
+                <li className='link'><a href="#about">About</a></li>
+                <li className='link'><a href="#experience">Experience</a></li>
+                <li className='link'><a href="#languages">Languages</a></li>
+                <li className='link'><a href="#portfolio">Portfolio</a></li>
+                <li className='link'><a href="#certifications">Certifications</a></li>
+                <li className='link'><a href="#contact">Contact</a></li>
             </ul>
 
             <div className='footer__socials'>
