b0b909305b803c8da1e4e41f14f7539f2623181f
Naman Bajaj <namanbajaj05@gmail.com>
2026-01-11 12:06:21 -0500

    journey

--- src/Pages/OMSCS/components/Semester/Semester.tsx
+++ src/Pages/OMSCS/components/Semester/Semester.tsx
@@ -4,7 +4,7 @@ import Class from "../Class/Class";
 
 export default function Semester({ title }: { title: string }) {
   return (
-    <section>
+    <section id="Semester">
       <div className="introtext"><h1>{title}</h1></div>
       <Class classlist={['Computer Networks']} urls={['https://omscs.gatech.edu/cs-6250-computer-networks']} />
     </section>
