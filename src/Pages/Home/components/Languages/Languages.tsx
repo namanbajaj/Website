a66cc03da48ab1a41374de4b357dcdc8481eccea
Naman Bajaj <namanbajaj05@gmail.com>
2026-01-17 14:34:49 -0500

    OMSCS

--- src/Pages/Home/components/Languages/Languages.tsx
+++ src/Pages/Home/components/Languages/Languages.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ScrollAnimation from 'react-animate-on-scroll';
 
 export default function Languages() {
   return (
     <section id='Languages'>
-      <div className='l_t_header'>
-        <h1>
-          Languages and Technologies
-        </h1>
-      </div>
-
-      <Link to="/tools" className='btn go_to_projects_link'>Check out the languages and tools I've used!</Link>
+      <ScrollAnimation animateIn="animate__animated animate__fadeInLeft" duration={1.5} animateOnce={true}>
+        <div className='l_t_header'>
+          <h1>
+            Languages and Technologies
+          </h1>
+        </div>
+      </ScrollAnimation>
+      
+      <ScrollAnimation animateIn="animate__animated animate__bounceIn" duration={1.5} animateOnce={true}>
+        <Link to="/tools" className='btn go_to_projects_link'>Check out the languages and tools I've used!</Link>
+      </ScrollAnimation>
     </section >
   )
 }
--- src/Pages/Home/components/Portfolio/Portfolio.tsx
+++ src/Pages/Home/components/Portfolio/Portfolio.tsx
@@ -4,15 +4,20 @@ import './portfolio.css'
 import 'animate.css';
 import { Link } from 'react-router-dom';
 
+import ScrollAnimation from 'react-animate-on-scroll';
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      <div className='portfolio_intro_text'>
-        <h1>Projects</h1>
-      </div>
-
-      <Link to="/projects" className='btn go_to_projects_link'>Check out my projects!</Link>
+      <ScrollAnimation animateIn="animate__animated animate__fadeInLeft" duration={1.5} animateOnce={true}>
+        <div className='portfolio_intro_text'>
+          <h1>Projects</h1>
+        </div>
+      </ScrollAnimation>
+      
+      <ScrollAnimation animateIn="animate__animated animate__bounceIn" duration={1.5} animateOnce={true}>
+        <Link to="/projects" className='btn go_to_projects_link'>Check out my projects!</Link>
+      </ScrollAnimation>
     </section >
   )
 }
